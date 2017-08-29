b9a9e45afaae3787164240f1996301ec627cf3e0
Pancuatico <pancreatico@hotmail.cl>
2017-08-29 19:32:56 -0300

    proyecto base
    hagan commits solo si tienen alguna versión estable de su avance

--- www/conexion.php
+++
@@ -1,6 +0,0 @@
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "phonegap";
-$con = mysql_connect($server, $username, $password) or die ("Error al conectar: " . mysql_error());
-mysql_select_db($database, $con);
