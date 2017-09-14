75610d5e069f699cb8f1af277d108a083129133b
ljara2013 <31144490+ljara2013@users.noreply.github.com>
2017-09-13 21:40:33 -0300

    eventos.php
    actualizacion

---
+++ backend/eventos.php
@@ -0,0 +1,45 @@
+<?php 
+//error_reporting(0); 
+
+//Nos conectamos a la base de datos
+include 'connect.php';
+
+//Inicio de variables de sesión
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
+
+    function endChildren() { 
+        echo "</tr>" . "\n";
+    } 
+}
+
+
+
+try {
+	$eventos=$db->prepare("SELECT * FROM `eventos`");
+	$eventos->execute();
+	
+    $result = $eventos->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($eventos->fetchAll())) as $k=>$v) { 
+        echo $v. "-" ;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
