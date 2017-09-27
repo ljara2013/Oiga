3e2c0d14d29e73fb1eac8e91584c171b2c5bb1f6
ljara2013 <31144490+ljara2013@users.noreply.github.com>
2017-09-26 23:01:54 -0300

    Update connect.php

--- backend/connect.php
+++ backend/connect.php
@@ -1,8 +1,8 @@
 <?php
 try {
-	$db="id2329550_losturnos";						//datos del hosting
-	$us="id2329550_pancreatico";
-	$pw="klampot564rico";
+	$db="id2746458_oiga";						//datos del hosting
+	$us="id2746458_lesly";
+	$pw="123456";
 	$params = 'mysql:host=localhost;dbname='.$db.';charset=utf8';
 	
 	$db = new PDO($params, $us, $pw);			//variable $db contiene la conexión PDO
