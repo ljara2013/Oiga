44db66e9b92b733776983ad8e2572211162128d9
ljara2013 <31144490+ljara2013@users.noreply.github.com>
2017-10-05 22:24:59 -0300

    Update EntregadorEvento.php

--- backend/EntregadorEvento.php
+++ backend/EntregadorEvento.php
@@ -16,7 +16,7 @@ $id= $_POST['id'];
 
 try {
 	 $seleccionar=$db->prepare("SELECT usuarios.nombre, eventos.titulo, usuarios.nombre, eventos.anonimo, eventos.tipo,eventos.descripcion, eventos.coordenadas, eventos.fecha FROM eventos INNER JOIN usuarios ON eventos.id_usuario=usuarios.id WHERE eventos.id=:id ");
-	$seleccionar->bindValue(':id',id,PDO::PARAM_INT);    //PDO::PARAM_INT para enteros
+	$seleccionar->bindValue(':id',$id,PDO::PARAM_INT);    //PDO::PARAM_INT para enteros
         $seleccionar->execute();
 	
         $resul = $seleccionar->fetchAll(PDO::FETCH_ASSOC);     //crea un arreglo con todos los datos     
